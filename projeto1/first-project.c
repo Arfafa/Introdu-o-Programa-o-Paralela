9e970ec3c392f4382317ea9b8b42c19b187f8124
Miguel Lopes <miguel.lopes.filho@usp.br>
2019-09-25 22:36:01 -0300

    Primeiro projeto finalizado. Preciso apenas rever o programa, fazer comentários. Além disso, este programa precisa ser otimizado

--- projeto1/first-project.c
+++ projeto1/first-project.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct node {
   int vertex;
   int total_neighbors;
-  // int *neighbors;
+  int *neighbors;
   struct node *next;
 
 } NODE;
@@ -17,7 +18,8 @@ typedef struct graph {
 } GRAPH;
 
 void insert(GRAPH *network, int *dupla);
-void write(GRAPH *network);
+void add_neighbor(NODE *elem, int viz);
+void write(GRAPH *network, char *file_name);
 void liberar(GRAPH *network);
 
 int main(int argc, char **argv) {
@@ -31,7 +33,6 @@ int main(int argc, char **argv) {
     file = fopen(argv[i], "r");
 
     if (file != NULL) {
-      printf("%s\n", argv[i]);
       count = 0;
 
       network->tamanho = 0;
@@ -47,7 +48,7 @@ int main(int argc, char **argv) {
         count++;
       }
 
-      write(network);
+      write(network, argv[i]);
 
       liberar(network);
       fclose(file);
@@ -69,6 +70,10 @@ void insert(GRAPH *network, int *dupla) {
     new->total_neighbors = 1;
     new->next = NULL;
 
+    new->neighbors = (int *)malloc(sizeof(int));
+
+    new->neighbors[0] = dupla[(i + 1) % 2];
+
     aux = network->inicio;
     corr = network->inicio;
 
@@ -80,8 +85,10 @@ void insert(GRAPH *network, int *dupla) {
 
     else {
       while (aux != NULL && aux->vertex <= new->vertex) {
-        if (aux->vertex == new->vertex)
+        if (aux->vertex == new->vertex) {
           aux->total_neighbors++;
+          add_neighbor(aux, new->neighbors[0]);
+        }
 
         corr = aux;
         aux = aux->next;
@@ -110,18 +117,67 @@ void insert(GRAPH *network, int *dupla) {
   }
 }
 
-void write(GRAPH *network) {
+void add_neighbor(NODE *elem, int viz) {
 
-  NODE *aux = network->inicio;
+  int i = 0, aux = viz, corr;
+
+  elem->neighbors =
+      (int *)realloc(elem->neighbors, elem->total_neighbors * sizeof(int));
+
+  while (i < elem->total_neighbors - 1 && elem->neighbors[i] < viz)
+    i++;
+
+  for (int j = i; j < elem->total_neighbors; j++) {
+    corr = elem->neighbors[j];
+    elem->neighbors[j] = aux;
+    aux = corr;
+  }
+}
+
+void write(GRAPH *network, char *file_name) {
+
+  NODE *aux = network->inicio, *corr;
+  int j, count, l = strlen(file_name) - 1;
+  FILE *file;
+
+  while (l >= 0 && file_name[l] != '.')
+    l--;
+
+  if (l >= 0)
+    file_name[l] = '\0';
+
+  strcat(file_name, ".cng");
+
+  file = fopen(file_name, "w");
 
   while (aux != NULL) {
-    printf("Node: %d\n", aux->vertex);
-    printf("Total de Vizinhos: %d\n", aux->total_neighbors);
+    corr = aux->next;
+
+    while (corr != NULL) {
+      count = 0;
+
+      for (int i = 0; i < aux->total_neighbors; i++) {
+        j = 0;
+
+        while (j < corr->total_neighbors &&
+               corr->neighbors[j] <= aux->neighbors[i]) {
+          if (corr->neighbors[j] == aux->neighbors[i])
+            count++;
+
+          j++;
+        }
+      }
+
+      if (count)
+        fprintf(file, "%d %d %d\n", aux->vertex, corr->vertex, count);
+
+      corr = corr->next;
+    }
 
     aux = aux->next;
   }
 
-  printf("Tamanho da rede: %d\n", network->tamanho);
+  fclose(file);
 }
 
 void liberar(GRAPH *network) {
