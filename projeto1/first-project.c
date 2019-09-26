847cfb23f1fd549baa56fdb83d2d2ea0dd88fc9d
Miguel Lopes <miguel.lopes.filho@usp.br>
2019-09-26 16:45:48 -0300

    Projeto melhor organizado. Falta comentários e otimização!

--- projeto1/first-project.c
+++ projeto1/first-project.c
@@ -10,23 +10,23 @@ typedef struct node {
 
 } NODE;
 
-typedef struct graph {
-  NODE *inicio;
-  NODE *fim;
-  int tamanho;
+typedef struct adjacency_list {
+  NODE *start;
+  NODE *end;
+  int size;
 
-} GRAPH;
+} ADJACENCY_LIST;
 
-void insert(GRAPH *network, int *dupla);
-void add_neighbor(NODE *elem, int viz);
-void write(GRAPH *network, char *file_name);
-void liberar(GRAPH *network);
+void insert(ADJACENCY_LIST *adlist, int *pair);
+void add_neighbor(NODE *vx, int neigh);
+void write(ADJACENCY_LIST *adlist, char *file_name);
+void liberate(ADJACENCY_LIST *adlist);
 
 int main(int argc, char **argv) {
-  GRAPH *network = (GRAPH *)malloc(sizeof(GRAPH));
+  ADJACENCY_LIST *adlist = (ADJACENCY_LIST *)malloc(sizeof(ADJACENCY_LIST));
   FILE *file;
   int n, count;
-  int dupla[2];
+  int pair[2];
 
   for (int i = 1; i < argc; i++) {
 
@@ -35,52 +35,52 @@ int main(int argc, char **argv) {
     if (file != NULL) {
       count = 0;
 
-      network->tamanho = 0;
-      network->inicio = NULL;
-      network->fim = NULL;
+      adlist->size = 0;
+      adlist->start = NULL;
+      adlist->end = NULL;
 
       while (fscanf(file, "%d", &n) != EOF) {
-        dupla[count % 2] = n;
+        pair[count % 2] = n;
 
         if (count % 2)
-          insert(network, dupla);
+          insert(adlist, pair);
 
         count++;
       }
 
-      write(network, argv[i]);
+      write(adlist, argv[i]);
 
-      liberar(network);
+      liberate(adlist);
       fclose(file);
     }
   }
 
-  free(network);
+  free(adlist);
 
   return 0;
 }
 
-void insert(GRAPH *network, int *dupla) {
-  NODE *aux, *corr;
+void insert(ADJACENCY_LIST *adlist, int *pair) {
+  NODE *aux, *chain;
 
   for (int i = 0; i < 2; i++) {
     NODE *new = (NODE *)malloc(sizeof(NODE));
 
-    new->vertex = dupla[i];
+    new->vertex = pair[i];
     new->total_neighbors = 1;
     new->next = NULL;
 
     new->neighbors = (int *)malloc(sizeof(int));
 
-    new->neighbors[0] = dupla[(i + 1) % 2];
+    new->neighbors[0] = pair[(i + 1) % 2];
 
-    aux = network->inicio;
-    corr = network->inicio;
+    aux = adlist->start;
+    chain = adlist->start;
 
-    if (network->inicio == NULL) {
-      network->inicio = new;
-      network->fim = new;
-      network->tamanho = 1;
+    if (adlist->start == NULL) {
+      adlist->start = new;
+      adlist->end = new;
+      adlist->size = 1;
     }
 
     else {
@@ -90,53 +90,53 @@ void insert(GRAPH *network, int *dupla) {
           add_neighbor(aux, new->neighbors[0]);
         }
 
-        corr = aux;
+        chain = aux;
         aux = aux->next;
       }
 
-      if (aux == NULL && corr->vertex < new->vertex) {
-        corr->next = new;
-        network->fim = new;
-        network->tamanho++;
+      if (aux == NULL && chain->vertex < new->vertex) {
+        chain->next = new;
+        adlist->end = new;
+        adlist->size++;
 
       }
 
       else if (aux != NULL && aux->vertex > new->vertex &&
-               corr->vertex < new->vertex) {
+               chain->vertex < new->vertex) {
         new->next = aux;
-        corr->next = new;
-        network->tamanho++;
+        chain->next = new;
+        adlist->size++;
       }
 
-      else if (aux != NULL && aux->vertex == network->inicio->vertex) {
-        new->next = network->inicio;
-        network->inicio = new;
-        network->tamanho++;
+      else if (aux != NULL && aux->vertex == adlist->start->vertex) {
+        new->next = adlist->start;
+        adlist->start = new;
+        adlist->size++;
       }
     }
   }
 }
 
-void add_neighbor(NODE *elem, int viz) {
+void add_neighbor(NODE *vx, int neigh) {
 
-  int i = 0, aux = viz, corr;
+  int i = 0, aux = neigh, chain;
 
-  elem->neighbors =
-      (int *)realloc(elem->neighbors, elem->total_neighbors * sizeof(int));
+  vx->neighbors =
+      (int *)realloc(vx->neighbors, vx->total_neighbors * sizeof(int));
 
-  while (i < elem->total_neighbors - 1 && elem->neighbors[i] < viz)
+  while (i < vx->total_neighbors - 1 && vx->neighbors[i] < neigh)
     i++;
 
-  for (int j = i; j < elem->total_neighbors; j++) {
-    corr = elem->neighbors[j];
-    elem->neighbors[j] = aux;
-    aux = corr;
+  for (int j = i; j < vx->total_neighbors; j++) {
+    chain = vx->neighbors[j];
+    vx->neighbors[j] = aux;
+    aux = chain;
   }
 }
 
-void write(GRAPH *network, char *file_name) {
+void write(ADJACENCY_LIST *adlist, char *file_name) {
 
-  NODE *aux = network->inicio, *corr;
+  NODE *aux = adlist->start, *chain;
   int j, count, l = strlen(file_name) - 1;
   FILE *file;
 
@@ -151,17 +151,17 @@ void write(GRAPH *network, char *file_name) {
   file = fopen(file_name, "w");
 
   while (aux != NULL) {
-    corr = aux->next;
+    chain = aux->next;
 
-    while (corr != NULL) {
+    while (chain != NULL) {
       count = 0;
 
       for (int i = 0; i < aux->total_neighbors; i++) {
         j = 0;
 
-        while (j < corr->total_neighbors &&
-               corr->neighbors[j] <= aux->neighbors[i]) {
-          if (corr->neighbors[j] == aux->neighbors[i])
+        while (j < chain->total_neighbors &&
+               chain->neighbors[j] <= aux->neighbors[i]) {
+          if (chain->neighbors[j] == aux->neighbors[i])
             count++;
 
           j++;
@@ -169,9 +169,9 @@ void write(GRAPH *network, char *file_name) {
       }
 
       if (count)
-        fprintf(file, "%d %d %d\n", aux->vertex, corr->vertex, count);
+        fprintf(file, "%d %d %d\n", aux->vertex, chain->vertex, count);
 
-      corr = corr->next;
+      chain = chain->next;
     }
 
     aux = aux->next;
@@ -180,16 +180,16 @@ void write(GRAPH *network, char *file_name) {
   fclose(file);
 }
 
-void liberar(GRAPH *network) {
-  NODE *aux, *corrente;
+void liberate(ADJACENCY_LIST *adlist) {
+  NODE *aux, *chain;
 
-  corrente = network->inicio;
+  aux = adlist->start;
 
-  while (corrente != NULL) {
-    aux = corrente->next;
+  while (aux != NULL) {
+    chain = aux->next;
 
-    free(corrente);
+    free(aux);
 
-    corrente = aux;
+    aux = chain;
   }
 }
